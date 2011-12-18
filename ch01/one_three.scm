1c8ccfa9dda9f8327c6cddba3f066a598da4f29e
Estevan Pequeno <estevan.pequeno@gmail.com>
2011-12-17 22:54:08 -0600

    cant figure out why one_three isnt working

--- ch01/one_three.scm
+++ ch01/one_three.scm
@@ -1,11 +1,11 @@
-;Define a procedure that takes three numbers as argu-
-;ments and returns the sum of the squares of the two larger num-
-;bers.
+;Define a procedure that takes three numbers as arguments and returns the 
+;sum of the squares of the two larger numbers.
 
 (define (square n) (* n n))
 
-(define (sum-of-squares a b c) (cond (= (min a b c) a) (+ (square b) (square c))
-                                     (= (min a b c) b) (+ (square a) (square c))
-                                     (else (+ (square a) (square b)))))
-(display (sum-of-squares 3 4 5))
+(define (sum-of-squares a b c) 
+(cond (= (min a b c) a) (+ (square b) (square c))
+      (= (min a b c) b) (+ (square a) (square c))
+      (else (+ (square a) (square b)))))
+(display (sum-of-squares 5 4 3))
 (newline)
