0aaef6dc3510c1cccbdc64a22ef87f7d8f6aa4dc
Estevan Pequeno <estevan.pequeno@gmail.com>
2012-12-20 10:03:18 -0600

    1.6 beat me

--- ch01/one_06.scm
+++ ch01/one_06.scm
@@ -19,3 +19,45 @@
 ; What happens when Alyssa attempts to use this to compute sqare
 ; roots? Explain.
 
+(define (square x) (* x x))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+;(define (sqrt-iter guess x)
+; (if (good-enough? guess x)
+;     guess
+;     (sqrt-iter (improve guess x) x)))
+
+ (define (sqrt-iter guess x)
+   (new-if (good-enough? guess x)
+           guess
+           (sqrt-iter (improve guess x) x)))
+
+;(display (if (> 1 0) 1 (/ 1 0)))
+(display (new-if (> 1 0) 1 (/ 1 0)))
+(newline)
+
+; Using new-if produces a stack overflow. This has to do with 
+; normal order execution vs applicative order. If we try to obtain
+; all the params of the cond in 'new-if' we recursivly try to call
+; 'sqrt-iter' endlessly. The built-in 'if' will break eventually when
+; 'good-enough?' returns #t. This is a tricky one to debug since different
+; interpreters will handle the operations differently. For example guile will
+; throw an error when we try to evaluate (new-if (> 1 0) 1 (/ 1 0)) which 
+; would throw a 'divide by 0' error if all params were evaluated but, scm does 
+; not. When loaded into scm, this file will return '1' with no errors with both
+; 'if' and 'new-if'. 
